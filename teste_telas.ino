53d120a5fc49a5cb985a8004b87c551623b73345
Thyago Ismael Oliveira da Silva <thyagoismael21@gmail.com>
2018-05-19 18:03:20 -0300

    teste alternancia de telas

--- teste_telas.ino
+++ teste_telas.ino
@@ -1,25 +1,78 @@
-/*
-  Este arquivo é apenas um teste para a
-  criacao do menu interativo do SHIU
-*/
-
 #include <LiquidCrystal.h>
 
-typedef struct t_eeprom
-{
-    int contador;
-    int tolerancia;
-    bool sensor_chave[NUM_SENSOR];        
-    short potenciometro_ideal[NUM_SENSOR];
-    byte tela_atual;    // adicionado para utilizar as telas
-}t_eeprom;
+#define TELA_INICIO 0
+#define TELA_MENU1 1
+#define TELA_MENU2 2
+#define UP 7
+#define DOWN 6
+#define ENTER 5
+#define BACK 4
 
-LiquidCrystal lcd(2, 3, 11, 10, 9, 8); // numeros arbitrarios
+LiquidCrystal lcd(2, 3, 11, 10, 9, 8);
+
+byte tela_atual = TELA_INICIO;
+byte botao_porta[4] = {UP, DOWN, ENTER, BACK};
+byte botao_estado[4] = {0};
+
+String texto_inicio[2] = 
+{
+  "Bemvindo ao SHIU",
+  "  Aperte ENTER  "
+};
+String texto_menu[4]= {
+  "  Ver sensores  ",
+  "  Ver potenciom ",
+  "  Historico     ",
+  "  Configuracoes "
+};
 
 void setup()
 {
+  lcd.begin(16,2);
+  lcd.noCursor();
+  
+  pinMode(13, OUTPUT);
 }
 
 void loop()
 {
+  escolher_tela();
+}
+void escolher_tela(void)
+{
+  switch(tela_atual)
+  {
+    case TELA_INICIO:
+    	tela_inicio();
+    	break;
+    case TELA_MENU1:
+    	lcd.clear();
+     	break;
+  }
+}
+bool ler_botao(byte sinal)
+{
+  byte tempo, atraso_debounce = 5;
+
+  if (digitalRead(botao_porta[sinal]) == HIGH)
+    tempo = millis();
+
+  if ((millis() - tempo) > atraso_debounce)
+    if (digitalRead(botao_porta[sinal]) == HIGH)
+		return true;
+    
+  return false;
+}
+void tela_inicio(void)
+{
+  lcd.setCursor(0, 0);
+  lcd.print(texto_inicio[0]);
+  lcd.setCursor(0, 1);
+  lcd.print(texto_inicio[1]);
+  
+  if (ler_botao(botao_porta[ENTER]))
+  {
+    digitalWrite(13, HIGH);
+    tela_atual = TELA_MENU1;
+  }
 }
