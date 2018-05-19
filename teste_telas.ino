f5a46e1a965e780c3a2faefdb3038b3d82d10cae
Thyago Ismael Oliveira da Silva <thyagoismael21@gmail.com>
2018-05-19 19:51:27 -0300

    Tela para leitura dos sensores adicionada #3

--- teste_telas.ino
+++ teste_telas.ino
@@ -1,8 +1,12 @@
 #include <LiquidCrystal.h>
 
+#define NUM_SENSOR 4
+
 #define TELA_INICIO 0
 #define TELA_MENU1 1
 #define TELA_MENU2 2
+#define TELA_VER_SENSOR 3
+
 #define PORTA_UP 7
 #define PORTA_DOWN 6
 #define PORTA_ENTER 5
@@ -12,7 +16,17 @@
 #define INDICE_ENTER 2
 #define INDICE_BACK 3
 
+typedef struct st_eeprom{
+  int contador;
+  int tolerancia;
+  bool sensor_chave[NUM_SENSOR];        
+  short potenciometro_ideal[NUM_SENSOR];
+}t_eeprom;
+
 LiquidCrystal lcd(2, 3, 11, 10, 9, 8);
+
+int  sensor_sinal[NUM_SENSOR]         = {10, 20, 30, 40};
+int  media_total                      = 25;
 
 byte tela_atual = TELA_INICIO;
 byte botao_porta[4] = {PORTA_UP, PORTA_DOWN, PORTA_ENTER, PORTA_BACK};
@@ -58,6 +72,8 @@ void escolher_tela(void)
     case TELA_MENU2:
     	tela_menu(2);
      	break;
+    case TELA_VER_SENSOR:
+    	tela_ver_sensor();
   }
 }
 void tela_inicio(void)
@@ -118,10 +134,49 @@ void tela_menu(byte tela)
     else if (posicao_escolha == 2)
       tela_atual = TELA_MENU2;
   }
-  else if (digitalRead(botao_porta[INDICE_BACK]))
+  
+  if (digitalRead(botao_porta[INDICE_ENTER]))
+  {
+    switch (posicao_escolha)
+    {
+      case 0:
+      	tela_atual = TELA_VER_SENSOR;
+      	tela_ver_sensor();
+      	break;
+      case 1:
+      	break;
+      case 2:
+      	break;
+      case 3:
+      	break;
+    }
+  }
+  
+  if (digitalRead(botao_porta[INDICE_BACK]))
   {
     posicao_escolha = 0;
     posicao_seta = 0;
     tela_atual = TELA_INICIO;
   }
+}
+void tela_ver_sensor(void)
+{
+  t_eeprom ep;
+  
+  for (int i = 0; i < NUM_SENSOR; i++)
+  {
+    lcd.setCursor(4*i, 0);
+    
+    if (ep.sensor_chave[i])
+    {
+      lcd.print(sensor_sinal[i]);
+      lcd.print("   ");
+    }
+    else
+      lcd.print("XXX ");
+  }
+  lcd.setCursor(0, 1);
+  lcd.print("Media           ");
+  lcd.setCursor(6, 1);
+  lcd.print(media_total);
 }
