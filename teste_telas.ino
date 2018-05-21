98a88f7a1882f544f0153a31ccb0c787b63b62e7
Thyago Ismael Oliveira da Silva <thyagoismael21@gmail.com>
2018-05-21 07:12:28 -0300

    Mudança na ordem das funções

--- teste_telas.ino
+++ teste_telas.ino
@@ -1,8 +1,8 @@
 #include <LiquidCrystal.h>
 #include <EEPROM.h>
 
-#define NUM_SENSOR 		  4
-#define NIVEL_LIMITE 	 75
+#define NUM_SENSOR 	 4
+#define NIVEL_LIMITE 	75
 #define DELAY_BOTAO 	50
 
 #define TELA_INICIO 0
@@ -146,6 +146,43 @@ void loop()
 {
   escolher_tela();  
 }
+/* ---------- Utilitarios -----------------*/
+void mostrar_seta(void)
+{
+  lcd.setCursor(0, posicao_escolha%2);
+  lcd.print("> ");
+  lcd.setCursor(0, !(posicao_escolha%2));
+  lcd.print("  ");
+}
+void limpa(byte linha)
+{
+  if (limpar_linha)
+  {
+  	lcd.setCursor(0, linha);
+  	lcd.print("                ");
+    limpar_linha = false;
+  }
+}
+bool ler_botao(int indice)
+{
+  static bool estado1, estado2;
+  static unsigned long tempo = millis();
+  
+  estado1 = digitalRead(botao_porta[indice]);
+  
+  if (millis() - tempo > DELAY_BOTAO)
+  {
+  	estado2 = digitalRead(botao_porta[indice]);
+    
+    if (estado1 == estado2 && estado1)
+    {
+      tempo = millis();
+      return true;
+    }
+  }
+  return false;
+}
+/* --------------------------------------- */
 void escolher_tela(void)
 {
   switch(tela_atual)
@@ -165,6 +202,9 @@ void escolher_tela(void)
     case TELA_VER_POT:
     	tela_ver_pot();
      	break;
+    case TELA_HISTORICO:
+    	tela_historico();
+    	break;
     case TELA_CONFIG1:
     	tela_config(1);
     	break;
@@ -177,22 +217,18 @@ void escolher_tela(void)
     case TELA_SENSIBILIDADE:
     	tela_sensibilidade();
     	break;
-    case TELA_POTENC_IDEAL:
-    	tela_potenc_ideal();
-    	break;
-	case TELA_RESETAR_CONFIG:
-    	tela_resetar_config();
-    	break;
     case TELA_SENSOR_ON_OFF:
     	tela_ligar_sensor();
     	break;
     case TELA_MODO_VARIACAO:
     	tela_modo_variacao();
     	break;
-    case TELA_HISTORICO:
-    	tela_historico();
+    case TELA_POTENC_IDEAL:
+    	tela_potenc_ideal();
     	break;
-    
+	case TELA_RESETAR_CONFIG:
+    	tela_resetar_config();
+    	break;    
   }
 }
 void tela_inicio(void)
@@ -323,6 +359,25 @@ void tela_ver_pot(void)
   if (ler_botao(BACK))
   	tela_atual = TELA_MENU1;
 }
+void tela_historico(void)
+{
+  t_eeprom ep;
+  EEPROM.get(0, ep);
+  
+  lcd.setCursor(0, 0);
+  lcd.print(texto_historico[0]);
+  
+  limpa(1);
+  
+  lcd.setCursor(0, 1);
+  lcd.print(ep.contador);
+  
+  if (ler_botao(BACK))
+  {
+    tela_atual = TELA_MENU1;
+    limpar_linha = true;
+  }
+}
 void tela_config(int tela)
 {
   mostrar_seta();
@@ -403,6 +458,7 @@ void tela_config(int tela)
     posicao_escolha = 0;
   }
 }
+
 void tela_sensibilidade(void)
 {
   bool executa = false;
@@ -456,6 +512,94 @@ void mod_tolerancia(char c) //modificar_tolerancia [funcao auxiliar eeprom]
   EEPROM.put(0, ep);
   
 }
+void tela_ligar_sensor(void)
+{
+  static char *icone = ">";
+  static bool iniciar_alteracao = false;
+  t_eeprom ep;
+  EEPROM.get(0, ep);
+  
+  lcd.setCursor(0, 0);
+  lcd.print(texto_ligar_sensor[0]);
+  
+  limpa(1);
+  
+  lcd.setCursor(4 * posicao_escolha, 0);
+  lcd.print(icone);
+  for (int i = 0; i < NUM_SENSOR;  i++)
+  {
+    lcd.setCursor(4 * i + 1, 1);
+    if (ep.sensor_chave[i])
+    	lcd.print(" on");
+    else
+      	lcd.print("off");
+  }
+  
+  if (!iniciar_alteracao)
+  {
+  	if (ler_botao(UP) && posicao_escolha < 3)
+    	posicao_escolha++;
+  	if (ler_botao(DOWN) && posicao_escolha > 0) 
+    	posicao_escolha--;
+  }
+  
+  if (ler_botao(ENTER))
+  {
+    icone = "#";
+    iniciar_alteracao = true;
+  }
+  
+  
+  if (iniciar_alteracao)
+  {
+    if (ler_botao(UP))
+		mod_chave(posicao_escolha, true);
+    else if (ler_botao(DOWN))
+		mod_chave(posicao_escolha, false);
+      
+    if (ler_botao(BACK))
+    {
+      icone = ">";
+      iniciar_alteracao = false;
+      posicao_escolha = 0;
+    }
+  }
+  else if (ler_botao(BACK))
+  {
+   	tela_atual = TELA_CONFIG1;
+   	limpar_linha = true;
+  }
+}
+void mod_chave(int sensor, bool status) //modificar_chave - do sensor [funcao auxiliar eeprom]
+{
+  t_eeprom ep;
+
+  EEPROM.get(0, ep);
+
+  ep.sensor_chave[sensor] = status;
+
+  EEPROM.put(0, ep);
+}
+void tela_modo_variacao(void)
+{
+  lcd.setCursor(1, 0);
+  lcd.print(texto_modo_variacao[0]);
+  lcd.setCursor(1, 1);
+  lcd.print(texto_modo_variacao[1]);
+ 
+  mostrar_seta();
+  
+  /* 
+  	Ambos os modos ainda nao estao definidos
+  */
+  
+  if (ler_botao(UP) || ler_botao(DOWN))
+    posicao_escolha = !posicao_escolha;
+    
+  if (ler_botao(BACK))
+    tela_atual = TELA_CONFIG1;
+}
+
 void tela_potenc_ideal(void)
 {
   static char *seta = ">";
@@ -553,146 +697,4 @@ void conf_padrao(void)//carrega a parte inicial do eeprom(endereco 0) com a stru
   }
 
   EEPROM.put(0, ep);
-}
-void tela_ligar_sensor(void)
-{
-  static char *icone = ">";
-  static bool iniciar_alteracao = false;
-  t_eeprom ep;
-  EEPROM.get(0, ep);
-  
-  lcd.setCursor(0, 0);
-  lcd.print(texto_ligar_sensor[0]);
-  
-  limpa(1);
-  
-  lcd.setCursor(4 * posicao_escolha, 0);
-  lcd.print(icone);
-  for (int i = 0; i < NUM_SENSOR;  i++)
-  {
-    lcd.setCursor(4 * i + 1, 1);
-    if (ep.sensor_chave[i])
-    	lcd.print(" on");
-    else
-      	lcd.print("off");
-  }
-  
-  if (!iniciar_alteracao)
-  {
-  	if (ler_botao(UP) && posicao_escolha < 3)
-    	posicao_escolha++;
-  	if (ler_botao(DOWN) && posicao_escolha > 0) 
-    	posicao_escolha--;
-  }
-  
-  if (ler_botao(ENTER))
-  {
-    icone = "#";
-    iniciar_alteracao = true;
-  }
-  
-  
-  if (iniciar_alteracao)
-  {
-    if (ler_botao(UP))
-		mod_chave(posicao_escolha, true);
-    else if (ler_botao(DOWN))
-		mod_chave(posicao_escolha, false);
-      
-    if (ler_botao(BACK))
-    {
-      icone = ">";
-      iniciar_alteracao = false;
-      posicao_escolha = 0;
-    }
-  }
-  else if (ler_botao(BACK))
-  {
-   	tela_atual = TELA_CONFIG1;
-   	limpar_linha = true;
-  }
-}
-void mod_chave(int sensor, bool status) //modificar_chave - do sensor [funcao auxiliar eeprom]
-{
-  t_eeprom ep;
-
-  EEPROM.get(0, ep);
-
-  ep.sensor_chave[sensor] = status;
-
-  EEPROM.put(0, ep);
-}
-void tela_modo_variacao(void)
-{
-  lcd.setCursor(1, 0);
-  lcd.print(texto_modo_variacao[0]);
-  lcd.setCursor(1, 1);
-  lcd.print(texto_modo_variacao[1]);
- 
-  mostrar_seta();
-  
-  /* 
-  	Ambos os modos ainda nao estao definidos
-  */
-  
-  if (ler_botao(UP) || ler_botao(DOWN))
-    posicao_escolha = !posicao_escolha;
-    
-  if (ler_botao(BACK))
-    tela_atual = TELA_CONFIG1;
-}
-void tela_historico(void)
-{
-  t_eeprom ep;
-  EEPROM.get(0, ep);
-  
-  lcd.setCursor(0, 0);
-  lcd.print(texto_historico[0]);
-  
-  limpa(1);
-  
-  lcd.setCursor(0, 1);
-  lcd.print(ep.contador);
-  
-  if (ler_botao(BACK))
-  {
-    tela_atual = TELA_MENU1;
-    limpar_linha = true;
-  }
-}
-
-bool ler_botao(int indice)
-{
-  static bool estado1, estado2;
-  static unsigned long tempo = millis();
-  
-  estado1 = digitalRead(botao_porta[indice]);
-  
-  if (millis() - tempo > DELAY_BOTAO)
-  {
-  	estado2 = digitalRead(botao_porta[indice]);
-    
-    if (estado1 == estado2 && estado1)
-    {
-      tempo = millis();
-      return true;
-    }
-  }
-  return false;
-}
-void mostrar_seta(void)
-{
-  lcd.setCursor(0, posicao_escolha%2);
-  lcd.print("> ");
-  lcd.setCursor(0, !(posicao_escolha%2));
-  lcd.print("  ");
-}
-void limpa(byte linha)
-{
-  if (limpar_linha)
-  {
-  	lcd.setCursor(0, linha);
-  	lcd.print("                ");
-    limpar_linha = false;
-  }
 }
