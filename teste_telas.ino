e834e36e115f07da83c8494355b747d8d583f653
Thyago Ismael Oliveira da Silva <thyagoismael21@gmail.com>
2018-05-21 09:41:19 -0300

    Comentários adicionados

--- teste_telas.ino
+++ teste_telas.ino
@@ -37,32 +37,127 @@ typedef struct st_eeprom{
   short potenciometro_ideal[NUM_SENSOR];
 }t_eeprom;
 
-void LCD_menu_start(void);
-void tela_inicio(void);
-void tela_menu(byte tela);
-void tela_ver_sensor(void);
-void tela_ver_pot(void);
-void tela_config(int tela);
+/* ------- Utilitarios ---------- */
 void mostrar_seta(void);
-void tela_sensibilidade(void);
-void mod_tolerancia(char c);
-void tela_potenc_ideal(void);
-void mod_pot_ideal(int pot, char botao);
-void tela_resetar_config(void);
-void conf_padrao(void);
-void tela_ligar_sensor(void);
-void mod_chave(int sensor, bool status);
-void tela_modo_variacao(void);
-void tela_historico(void);
+/*
+	Mostra na tela o icone da seta 
+    conforme a variavel posicao_escolha
+*/
 bool ler_botao(int indice);
+/*
+	Le o botao informado com debounce.
+    Retorna verdadeiro caso a leitura seja HIGH,
+    logo e preciso utilizar resitstores pulldown
+*/
 void limpa(byte linha);
+/*
+	Caso a flag limpar_linha seja verdadeira,
+    substitui a linha indicada por espacos em branco
+*/
+/* ------------------------------ */
+
+
+/* ------------ Funcoes de tela ----------------- */
+void LCD_menu_start(void);
+/*
+	Unica funcao que precisa ser chamada no
+    codigo real. Ela usa a tela_atual para
+    decidir qual funcao de tela deve ser
+    chamada.
+    Deve ser chamada na loop().
+*/
+void tela_inicio(void);
+/*
+	Tela de boas-vindas
+*/
+void tela_menu(byte tela);
+/*
+	Primeiro menu:
+    [0] Ver sensor
+    [1] Ver potenciometro
+    [2] Historico (quantas vezes a sirene apitou)
+    [3] Configuracoes
+*/
+void tela_ver_sensor(void);
+/*
+	Mostra a leitura de todos os sensores que tiverem
+    sensor_chave = true (nos que tiverem false sera 
+    mostrado "off" no lugar).
+    Eh mostrado tambem a media_total
+*/
+void tela_ver_pot(void);
+/*
+	Mostra a leitura de todos os potenciometros,
+    a tolerancia (eeprom) e o valor do potenciometro
+    ideal (eeprom)
+*/
+void tela_historico(void);
+/*
+	Mostra na tela quantas vezes a sirene foi
+    disparada (ep.contador).
+*/
+void tela_config(int tela);
+/*
+	Segundo menu:
+    [0] Sensibilidade
+    [1] Sensor on-off
+    [2] Modo de variacao
+    [3] Potenciometro ideal
+    [4] Resetar configuracoes
+*/
+void tela_sensibilidade(void);
+/*
+	Permite alterar ep.tolerancia
+*/
+void mod_tolerancia(char c);
+/*
+	Funcao definida no codigo original
+    Usada em tela_sensibilidade
+*/
+void tela_ligar_sensor(void);
+/*
+	Permite alterar ep.sensor_chave
+*/
+void mod_chave(int sensor, bool status);
+/*
+	Funcao definida no codigo original
+    Usada em tela_ligar_sensor
+*/
+void tela_modo_variacao(void);
+/*
+	Ainda nao esta definido como os modos
+    apresentados irao funcionar.
+    Esta funcao apenas mostra texto na tela
+*/
+void tela_potenc_ideal(void);
+/*
+	Permite alterar ep.potenciometro_ideal
+*/
+void mod_pot_ideal(int pot, char botao);
+/*
+	Funcao definida no codigo original
+    Usada em tela_potenc_ideal
+*/
+void tela_resetar_config(void);
+/*
+	Permite alterar todo o eeprom para as
+    configuracoes originais
+*/
+void conf_padrao(void);
+/*
+	Funcao definida no codigo original
+    Usada em tela_resetar_config
+*/
+/* ------------------------------------------ */
 
 LiquidCrystal lcd(2, 3, 11, 10, 9, 8);
 
+// Variaveis do codigo original
 int  sensor_sinal[NUM_SENSOR]         = {10, 20, 30, 40};
 int  potenciometro_sinal[NUM_SENSOR]  = {100, 200, 300, 400};
 int  media_total                      = 25;
 
+// Caractere definido para a seta
 byte seta[8] = {
   B00000,
   B00100,
@@ -73,13 +168,33 @@ byte seta[8] = {
   B00000,
 };
 
+/* ------- Variaveis deste codigo ------- */
 byte tela_atual = TELA_INICIO;
+/*
+	Variavel usada para determinar 
+    qual funcao sera executada
+*/
 byte botao_porta[4] = {PORTA_UP, PORTA_DOWN, PORTA_ENTER, PORTA_BACK};
-byte botao_estado[4] = {0};
+/*
+	Define quais sao as portas do
+    arduino onde estao os botoes
+*/
 int  posicao_escolha = 0;
+/*
+	Conforme o usuario vai navegando pelo
+    menu, esta variavel eh alterada. Ela
+    corresponde ao numero da escolha.
+*/
 bool limpar_linha = true;
+/*
+	Flag utilizada na funcao limpa(int linha).
+    Ela garante que a linha escolhida sera
+    substiuida por espacos em branco uma unica
+    vez, garantindo que a tela nao fique piscando.
+*/
 
 
+/* ------ Textos mostrados na tela -------- */
 String texto_inicio[2] = 
 {
   "Bemvindo ao SHIU",
@@ -131,14 +246,17 @@ String texto_historico[2] =
   "Qntd de sirenes ",
   "                "
 };
+/* ---------------------------------------- */
 
 void setup()
 {
   t_eeprom ep;
   
+  /* Inicializacao do LCD */
   lcd.createChar(0, seta);
   lcd.begin(16,2);
   lcd.noCursor();
+  /* -------------------- */
   
   for(int i=0; i<NUM_SENSOR; i++)
   {
@@ -154,16 +272,25 @@ void setup()
 
 void loop()
 {
-  LCD_menu_start();  
+  LCD_menu_start();  // Apenas essa funcao precisa ser chamada
 }
+
 /* ---------- Utilitarios -----------------*/
 void mostrar_seta(void)
 {
   lcd.setCursor(0, posicao_escolha%2);
-  lcd.write(byte(0));
+  lcd.write(byte(0)); // imprime o caractere da seta
   lcd.print(" ");
   lcd.setCursor(0, !(posicao_escolha%2));
   lcd.print("  ");
+  
+  /*
+  	  Quando eh tirado o modulo 2 os valores possiveis
+      sao 0 ou 1, exatamente o numero das linhas.
+      A ! inverte o valor da linha onde vai ser impressa
+      a seta, ou seja em uma linha eh impressa uma seta
+      e na outra um espaco em branco
+  */
 }
 void limpa(byte linha)
 {
@@ -173,12 +300,25 @@ void limpa(byte linha)
   	lcd.print("                ");
     limpar_linha = false;
   }
+  /*
+  	  Se a flag limpar_tela for verdadeira a linha
+      informada eh substituida por espacos em branco
+      e a flag se torna falsa. Isso garante que a linha
+      sera substuida apenas uma vez, evitando de o texto
+      ficar piscando na tela.
+  */
 }
 bool ler_botao(int indice)
 {
   static bool estado1, estado2;
+  /*
+  	Neste codigo, estado precisa ser static mas 
+    provavelmente no codigo real nao
+  */
   static unsigned long tempo = millis();
   
+  
+  /* --------------- Debounce --------------- */
   estado1 = digitalRead(botao_porta[indice]);
   
   if (millis() - tempo > DELAY_BOTAO)
@@ -196,6 +336,7 @@ bool ler_botao(int indice)
 /* --------------------------------------- */
 void LCD_menu_start(void)
 {
+  /* Esta funcao escolhe qual sera chamada */
   switch(tela_atual)
   {
     case TELA_INICIO:
@@ -244,6 +385,7 @@ void LCD_menu_start(void)
 }
 void tela_inicio(void)
 {
+  /* Tela de boas vindas */
   lcd.setCursor(0, 0);
   lcd.print(texto_inicio[0]);
   lcd.setCursor(0, 1);
@@ -251,11 +393,13 @@ void tela_inicio(void)
  
   if (ler_botao(ENTER))
     tela_atual = TELA_MENU1;
+  /* Apenas o botao ENTER eh reconhecido */
 }
 void tela_menu(byte tela)
 {
   mostrar_seta();
   
+  /* Duas possiveis telas */
   if (tela == 1)
   {
     lcd.setCursor(1, 0);
@@ -270,7 +414,9 @@ void tela_menu(byte tela)
     lcd.setCursor(1, 1);
     lcd.print(texto_menu[3]);
   }
+  /* --------------------- */
 
+  /* ------- Navegacao ------- */
   if (ler_botao(UP))
   {
     posicao_escolha--;
@@ -291,8 +437,9 @@ void tela_menu(byte tela)
     if (posicao_escolha == 4)
       posicao_escolha = 3;
   }
+  /* -------------------------- */
   
-  
+  /* --------- Escolha -------- */
   if (ler_botao(ENTER))
   {
     switch (posicao_escolha)
@@ -312,18 +459,22 @@ void tela_menu(byte tela)
     }
     posicao_escolha = 0;
   }
+  /* -------------------------- */
   
+  /* -------- Retorno --------- */
   if (ler_botao(BACK))
   {
     posicao_escolha = 0;
     tela_atual = TELA_INICIO;
   }
+  /* -------------------------- */
 }
 void tela_ver_sensor(void)
 {
   t_eeprom ep;
   EEPROM.get(0, ep);
   
+  /* ------- Mostra valores ------- */
   for (int i = 0; i < NUM_SENSOR; i++)
   {
     lcd.setCursor(4*i, 0);
@@ -336,39 +487,52 @@ void tela_ver_sensor(void)
     else
       lcd.print("off ");
   }
+  /* ----------------------------- */
   
-  limpa(1);
+  limpa(1); // limpa a linha 1
   
+  /* --- Mostra a media_total ---- */
   lcd.setCursor(0, 1);
   lcd.print("Media ");
   lcd.setCursor(6, 1);
   lcd.print(media_total);
+  /* ----------------------------- */
   
+  
+  /* ------- Retorno ------ */
   if (ler_botao(BACK))
   {
     tela_atual = TELA_MENU1;
     limpar_linha = true;
   }
+  /* ---------------------- */
 }
 void tela_ver_pot(void)
 {
   t_eeprom ep;
   EEPROM.get(0, ep);
-    
+  
+  /* ------- Mostra valores ------- */
   for (int i = 0; i < 4; i++)
   {
     lcd.setCursor(4*i, 0);
     lcd.print(potenciometro_sinal[i]);
     lcd.print("  ");
   }
+  /* ------------------------------ */
+  
+  /* Mostra a tolerancia e o valor ideal */
   lcd.setCursor(0, 1);
   lcd.print("Tol ");
   lcd.print(ep.tolerancia);
   lcd.print(" Ideal ");
   lcd.print(ep.potenciometro_ideal[0]);
+  /* ---------------------------------- */
   
+  /* ------- Retorno ------ */
   if (ler_botao(BACK))
   	tela_atual = TELA_MENU1;
+  /* ---------------------- */
 }
 void tela_historico(void)
 {
@@ -378,20 +542,26 @@ void tela_historico(void)
   lcd.setCursor(0, 0);
   lcd.print(texto_historico[0]);
   
-  limpa(1);
+  limpa(1); // limpa a linha 1
   
+  /* Mostra o valor do contador */
   lcd.setCursor(0, 1);
   lcd.print(ep.contador);
+  /* -------------------------- */
   
+  /* ------- Retorno ------ */
   if (ler_botao(BACK))
   {
     tela_atual = TELA_MENU1;
     limpar_linha = true;
   }
+  /* ---------------------- */
 }
 void tela_config(int tela)
 {
   mostrar_seta();
+  
+  // Tem 3 telas possiveis
   switch (tela)
   {
     case 1:
@@ -414,6 +584,8 @@ void tela_config(int tela)
     	break;
   }
   
+  
+  /* ----------- Navegacao -----------*/
   if (ler_botao(UP))
   {
     posicao_escolha--;
@@ -438,7 +610,9 @@ void tela_config(int tela)
     if (posicao_escolha == 5)
       posicao_escolha = 4;
   }
+  /* -------------------------------- */
   
+  /* ----------- Escolha ------------ */
   else if (ler_botao(ENTER))
   {
     
@@ -463,29 +637,44 @@ void tela_config(int tela)
     posicao_escolha = 0;
     
   }
+  /* -------------------------------- */
+  
+  /* ----------- Retorno ------------ */
   else if (ler_botao(BACK))
   {
     tela_atual = TELA_MENU1;
     posicao_escolha = 0;
   }
+  /* -------------------------------- */
 }
 
 void tela_sensibilidade(void)
 {
   bool executa = false;
+  /*
+  	A flag executa garante que o valor sera
+    alterado uma vez a cada leitura do botao.
+    Ela eh necessaria para evitar que, ao 
+    pressionar o botao, o numero cresca ou 
+    diminua indefinidamente.
+  */
   char alteracao;
   t_eeprom ep;
   EEPROM.get(0, ep);
   
-  limpa(0);
+  limpa(0); // limpa a linha 0
   
+  /* ----- Mostra valor na tela ----- */
   lcd.setCursor(0, 0);
   lcd.print(texto_sensibilidade[0]);
   lcd.setCursor(6, 0);
   lcd.print(ep.tolerancia);
   lcd.setCursor(0, 1);
   lcd.print(texto_sensibilidade[1]);
+  /* -------------------------------- */
   
+  
+  /* Le os botoes para definir a alteracao */
   if (ler_botao(UP))
   {
     alteracao = '+';
@@ -496,6 +685,7 @@ void tela_sensibilidade(void)
     alteracao = '-';
     executa = true;
   }
+  /* ------------------------------------- */
   
   if (executa)
   {
@@ -503,11 +693,13 @@ void tela_sensibilidade(void)
     executa = false;
   }
   
+  /* ------- Retorno ------ */
   if (ler_botao(BACK))
   {
     tela_atual = TELA_CONFIG1;
     limpar_linha = true;
   }
+  /* ---------------------- */
 }
 void mod_tolerancia(char c) //modificar_tolerancia [funcao auxiliar eeprom]
 {
@@ -525,18 +717,36 @@ void mod_tolerancia(char c) //modificar_tolerancia [funcao auxiliar eeprom]
 }
 void tela_ligar_sensor(void)
 {
+  /* 
+  	Esta funcao mostra os estados de todos os
+    sensores numa unica tela. A navegacao eh
+    identica a dos outros menus com a diferenca
+    que para alterar um valor, deve-se apertar ENTER
+    (o caractere '>' sera substituido por '#') e usar
+    os mesmos botoes de navegacao.
+  */
+  
+  
   static char *icone = ">";
   static bool iniciar_alteracao = false;
+  /*
+  	Flag para realizar a funcao apenas uma vez
+    a cada pressionar do botao
+  */
   t_eeprom ep;
   EEPROM.get(0, ep);
   
+  /* --- Mostra texto e o cursor na tela --- */
   lcd.setCursor(0, 0);
   lcd.print(texto_ligar_sensor[0]);
   
-  limpa(1);
+  limpa(1); // limpa a linha 1
   
   lcd.setCursor(4 * posicao_escolha, 0);
   lcd.print(icone);
+  /* --------------------------------------- */
+  
+  /* --- Mostra os estados dos sensores ---- */
   for (int i = 0; i < NUM_SENSOR;  i++)
   {
     lcd.setCursor(4 * i + 1, 1);
@@ -545,22 +755,34 @@ void tela_ligar_sensor(void)
     else
       	lcd.print("off");
   }
+  /* --------------------------------------- */
   
+  /* ------------- Navegacao --------------- */
   if (!iniciar_alteracao)
   {
   	if (ler_botao(UP) && posicao_escolha < 3)
     	posicao_escolha++;
   	if (ler_botao(DOWN) && posicao_escolha > 0) 
     	posicao_escolha--;
+    /*
+    	Os conectivos logicos evitam quem a 
+        variavel posicao_escolha receba valores
+        indevidos.
+    */
   }
+  /* --------------------------------------- */
   
+  /* ------- Escolha ------- */
   if (ler_botao(ENTER))
   {
     icone = "#";
+    // o icone eh alterado para indicar
+    // a escolha
     iniciar_alteracao = true;
   }
+  /* ----------------------- */
   
-  
+  /* ------------ Alteracao ------------ */
   if (iniciar_alteracao)
   {
     if (ler_botao(UP))
@@ -575,11 +797,15 @@ void tela_ligar_sensor(void)
       posicao_escolha = 0;
     }
   }
+  /* ----------------------------------- */
+  
+  /* ------- Retorno ------ */
   else if (ler_botao(BACK))
   {
    	tela_atual = TELA_CONFIG1;
    	limpar_linha = true;
   }
+  /* ---------------------- */
 }
 void mod_chave(int sensor, bool status) //modificar_chave - do sensor [funcao auxiliar eeprom]
 {
@@ -604,48 +830,77 @@ void tela_modo_variacao(void)
   	Ambos os modos ainda nao estao definidos
   */
   
-  if (ler_botao(UP) || ler_botao(DOWN))
+  /* ------------ Navegacao -----------*/
+  if (ler_botao(UP) && posicao_escolha == 1)
     posicao_escolha = !posicao_escolha;
-    
+  else if (ler_botao(DOWN) && posicao_escolha == 0)
+    posicao_escolha = !posicao_escolha;  
+  /* --------------------------------- */
+  
+  /* -------- Retorno -------- */
   if (ler_botao(BACK))
     tela_atual = TELA_CONFIG1;
+  /* ------------------------- */
 }
 
 void tela_potenc_ideal(void)
 {
-  static char *seta = ">";
+  /*
+  	Esta tela funciona exatamete como
+    a tela_ligar_sensor
+  */
+  
+  static char *icone = ">";
   static bool iniciar_alteracao = false;
+  /*
+  	Flag para realizar a funcao apenas uma vez
+    a cada pressionar do botao
+  */
   t_eeprom ep;
   EEPROM.get(0, ep);
   
+  /* --- Mostra texto e o cursor na tela --- */
   lcd.setCursor(0, 0);
   lcd.print(texto_potenc_ideal[0]);
   
-  limpa(1);
+  limpa(1); // limpa a linha 1
   
   lcd.setCursor(4 * posicao_escolha, 0);
-  lcd.print(seta);
+  lcd.print(icone);
+  /* --------------------------------------- */
+  
+  /* Mostra as leituras dos potenciometros */
   for (int i = 0; i < NUM_SENSOR;  i++)
   {
     lcd.setCursor(4 * i + 1, 1);
     lcd.print(ep.potenciometro_ideal[i]);
   }
+  /* --------------------------------------- */
   
+  /* ------------- Navegacao --------------- */
   if (!iniciar_alteracao)
   {
   	if (ler_botao(UP) && posicao_escolha < 3)
     	posicao_escolha++;
   	if (ler_botao(DOWN) && posicao_escolha > 0) 
     	posicao_escolha--;
+    /*
+    	Os conectivos logicos evitam quem a 
+        variavel posicao_escolha receba valores
+        indevidos.
+    */
   }
+  /* --------------------------------------- */
   
+  /* ------- Escolha ------- */
   if (ler_botao(ENTER))
   {
     seta = "#";
     iniciar_alteracao = true;
   }
+  /* ----------------------- */
   
-  
+  /* ------------ Alteracao ------------ */
   if (iniciar_alteracao)
   {
     if (ler_botao(UP))
@@ -660,11 +915,15 @@ void tela_potenc_ideal(void)
       posicao_escolha = 0;
     }
   }
+  /* ----------------------------------- */
+  
+  /* ------- Retorno ------ */
   else if (ler_botao(BACK))
   {
    	tela_atual = TELA_CONFIG1;
    	limpar_linha = true;
   }
+  /* ---------------------- */
 }
 void mod_pot_ideal(int pot, char botao) //modificar_potenciometro_ideal [funcao auxiliar eeprom]
 {
@@ -681,18 +940,25 @@ void mod_pot_ideal(int pot, char botao) //modificar_potenciometro_ideal [funcao 
 }
 void tela_resetar_config(void)
 {
+  /* -------Mostra texto --------- */
   lcd.setCursor(0, 0);
   lcd.print(texto_resetar_config[0]);
   lcd.setCursor(0, 1);
   lcd.print(texto_resetar_config[1]);
+  /* ----------------------------- */
   
+  /* --------- Escolha ----------- */
   if (ler_botao(ENTER))
   {
     conf_padrao();
     tela_atual = TELA_INICIO;
   }
+  /* ----------------------------- */
+  
+  /* ------ Retorno -------*/
   else if (ler_botao(BACK))
     tela_atual = TELA_CONFIG1;
+  /* --------------------- */
 }
 void conf_padrao(void)//carrega a parte inicial do eeprom(endereco 0) com a struct das informacoes do eeprom
 {                     //modifica todas as configuracoes para "configuracoes de fabrica"
